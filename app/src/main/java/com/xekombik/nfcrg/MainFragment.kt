fbf5e389253440b54d50b23d4464182b5e08ae93
Xek0mb1k <oganes969@gmail.com>
2023-04-09 12:58:56 +0300

    End base UI

--- app/src/main/java/com/xekombik/nfcrg/MainFragment.kt
+++ app/src/main/java/com/xekombik/nfcrg/MainFragment.kt
@@ -1,24 +1,60 @@
 package com.xekombik.nfcrg
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.VISIBLE
 import android.view.ViewGroup
-
-
-
-
+import com.xekombik.nfcrg.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
 
-    override fun onCreateView(
+    private val binding get() = _binding!!
+
+        override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        val animCircle = binding.animationCircle.background as AnimationDrawable
+        binding.sendDataButton.setOnClickListener {
+            sendData()
+
+
+            animCircle.start()
+        }
+
+        binding.readDataButton.setOnClickListener {
+            readData()
+            animCircle.start()
+        }
+
+
+        return binding.root
     }
 
+    private fun sendData() {
+        binding.animationCircle.visibility = VISIBLE
+        binding.modeTextView.visibility = VISIBLE
+        binding.modeTextView.text = getText(R.string.data_sending)
+        binding.editText.hint = "Enter text..."
+    }
 
+    private fun readData() {
+        binding.animationCircle.visibility = VISIBLE
+        binding.modeTextView.visibility = VISIBLE
+        binding.modeTextView.text = getText(R.string.data_reading)
+        binding.editText.hint = "Data will be here"
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
